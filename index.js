8c0af062e47bdecd98690eafaf818cddc1cfa0c7
Jede <brahmajede@gmail.com>
2021-03-14 10:37:11 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b) {
+    let result = a + b;
+    return result;
+    add(1,80)
+}
+function subtract(a, b) {
+    let result = a - b;
+    return result;
+    subtract(60, 40)
+}
+function multiply(a, b) {
+    let result = a * b
+    return result;
+    multiply(2, 3.4);
+}
+function divide(a, b) {
+    let result = a / b
+    return result;
+    divide(5.0, 2.5);
+}
+
+ 
+function increment(n) {
+    return n += 1
+  }
+  
+  function decrement(n) {
+    return n -= 1
+  }
+
+  
+  function makeInt(string) {
+    return parseInt(string, 10)
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string)
+  }
+  
